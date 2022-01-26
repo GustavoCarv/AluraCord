be369988238b77c40308640d612af33b670d07fc
GustavoCarv <gustavo.carvg@gmail.com>
2022-01-25 21:02:26 -0300

    Criado controle para usuario com useState e inicada a segunda pagina

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,9 @@
 import { Box, Button, Text, TextField, Image } from '@skynexui/components';
 import appConfig from '../config.json';
+import React, {useState} from 'react';
+import { useRouter } from 'next/router';
 
-function GlobalStyle() {
-  return (
-    <style global jsx>{`
-      * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        list-style: none;
-      }
-      body {
-        font-family: 'Open Sans', sans-serif;
-      }
-      /* App fit Height */ 
-      html, body, #__next {
-        min-height: 100vh;
-        display: flex;
-        flex: 1;
-      }
-      #__next {
-        flex: 1;
-      }
-      #__next > * {
-        flex: 1;
-      }
-      /* ./App fit Height */ 
-    `}</style>
-  );
-}
+
 
 function Titulo(props) {
   const Tag = props.tag || 'h1';
@@ -46,30 +21,21 @@ function Titulo(props) {
   );
 }
 
-// Componente React
-// function HomePage() {
-//     // JSX
-//     return (
-//         <div>
-//             <GlobalStyle />
-//             <Titulo tag="h2">Boas vindas de volta!</Titulo>
-//             <h2>Discord - Alura Matrix</h2>
-//         </div>
-//     )
-// }
-// export default HomePage
+
 
 export default function PaginaInicial() {
-  const username = 'peas';
+  const [username, setUsername] = useState('GustavoCarv');
+  const route = useRouter();
+  
 
   return (
     <>
-      <GlobalStyle />
+     
       <Box
         styleSheet={{
           display: 'flex', alignItems: 'center', justifyContent: 'center',
           backgroundColor: appConfig.theme.colors.primary[500],
-          backgroundImage: 'url(https://virtualbackgrounds.site/wp-content/uploads/2020/08/the-matrix-digital-rain.jpg)',
+          backgroundImage: 'url(https://images2.alphacoders.com/116/1165946.jpg)',
           backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundBlendMode: 'multiply',
         }}
       >
@@ -83,13 +49,18 @@ export default function PaginaInicial() {
               sm: 'row',
             },
             width: '100%', maxWidth: '700px',
-            borderRadius: '5px', padding: '32px', margin: '16px',
+            borderRadius: '10px', padding: '32px', margin: '16px',
             boxShadow: '0 2px 10px 0 rgb(0 0 0 / 20%)',
-            backgroundColor: appConfig.theme.colors.neutrals[700],
+            backgroundColor: "rgba( 255, 255, 255, 0.25 )",
+            backdropFilter: 'blur( 5.5px )',
           }}
         >
           {/* Formulário */}
           <Box
+            onSubmit={(e) => {
+              e.preventDefault();
+              route.push('/chat', { username });                          
+            }}
             as="form"
             styleSheet={{
               display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
@@ -102,6 +73,12 @@ export default function PaginaInicial() {
             </Text>
 
             <TextField
+              onChange={(e) => {
+                if(e.target.value.length > 0) {
+                 setUsername(e.target.value);
+                }  
+              }}
+              value={username}
               fullWidth
               textFieldColors={{
                 neutral: {
